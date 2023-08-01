ecea909e3cfa291b664bfe05381fba6ac41d7b35
Ranveer kumar <89473991+Ranveer87889@users.noreply.github.com>
2023-08-01 18:29:44 +0530

    Time: 24 ms (96.76%), Space: 30.9 MB (32.68%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -10,12 +10,42 @@
  * };
  */
 
-// algorithm tht is o(N)
+// algorithm tht is less than o(N)
 class Solution {
 public:
-  //  int count=0;
+     pair<bool,int> isPerfect(TreeNode* root){
+         int leftHeight =0;
+         int rightHeight =0;
+         TreeNode* temp = root;
+         while(temp){
+             leftHeight++;
+             temp=temp->left;
+         }
+         temp=root;
+         while(temp){
+             rightHeight++;
+             temp=temp->right;
+         }
+         return {leftHeight==rightHeight,leftHeight};
+     }
     int countNodes(TreeNode* root) {
         if(root==NULL) return 0;
-        return 1+countNodes(root->left)+countNodes(root->right);
+       pair<bool,int>p1=isPerfect(root->left);
+        pair<bool,int>p2 =isPerfect(root->right);
+        int count =1;
+          if(p1.first && p2.first){
+            count = 1+pow(2,p1.second)-1 + pow(2,p2.second)-1;
+              return count;
+        }
+        if(p1.first){
+            count+=pow(2,p1.second)-1+ countNodes(root->right);
+        
+        }
+         if(p2.first){
+            count += pow(2,p2.second)-1+countNodes(root->left);
+           
+        }
+      
+       return count;
     }
 };

94f7c84ef256e6b32a21c2806c76898888f81b38
Ranveer kumar <89473991+Ranveer87889@users.noreply.github.com>
2023-09-25 18:26:18 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (16.46%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
        int arr1[26]={0};
-        int arr2[26]={0};
         for(int i=0;i<s.size();i++){
             arr1[s[i]-'a']++;
         }
          for(int i=0;i<t.size();i++){
-            arr2[t[i]-'a']++;
+            arr1[t[i]-'a']--;
         }
          for(int i=0;i<26;i++){
-            if(arr1[i]!=arr2[i]) return 97+i;
+           if(arr1[i]!=0)return 97+i;
         }
         return '1';
        
